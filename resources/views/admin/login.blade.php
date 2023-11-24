3ddb001f623f20e516c6a864319cc652d383ef05
mayulondhe16 <mayulondhe16@gmail.com>
2023-11-24 14:29:27 +0530

    Testing Push Code

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -47,7 +47,7 @@
             <div class="card z-index-0 fadeIn3 fadeInBottom">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-primary shadow-primary border-radius-lg py-1 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-1 mb-1">Sign in</h4>
+                  <h4 class="text-white font-weight-bolder text-center mt-1 mb-1">Sign inn</h4>
                 </div>
               </div>
               <div class="card-body">
